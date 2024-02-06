038a80a439d79e1088b979c6f89006c2119112c0
gshekhawat0820 <gshekh@umich.edu>
2024-02-06 11:34:40 -0500

    launch screen and barebones assistant selection

---
+++ EchoHub/AssistantSelectView.swift
@@ -0,0 +1,88 @@
+//
+//  ContentView.swift
+//  EchoHub
+//
+//  Created by Gaurav Shekhawat on 2/5/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct AssistantSelectView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var assistants: [Assistant]
+
+    var body: some View {
+        NavigationStack {
+            VStack(spacing: 100) {
+                NavigationLink(destination: {
+                    AlexaView()
+                }, label: {
+                    Text("Amazon Alexa")
+                });
+                NavigationLink(destination: {
+                    GoogleView()
+                }, label: {
+                    Text("Google Home")
+                });
+            }
+            .navigationTitle("Assistant Selection")
+        }
+//        NavigationSplitView {
+//            List {
+//                ForEach(items) { item in
+//                    NavigationLink {
+//                        Text("Poop at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+//                    } label: {
+//                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+//                    }
+//                }
+//                .onDelete(perform: deleteItems)
+//            }
+//            .toolbar {
+//                ToolbarItem(placement: .navigationBarTrailing) {
+//                    EditButton()
+//                }
+//                ToolbarItem {
+//                    Button(action: addItem) {
+//                        Label("Add Item", systemImage: "plus")
+//                    }
+//                }
+//            }
+//        } detail: {
+//            Text("Select an item")
+//        }
+    }
+
+//    private func addItem() {
+//        withAnimation {
+//            let newItem = Item(timestamp: Date())
+//            modelContext.insert(newItem)
+//        }
+//    }
+//
+//    private func deleteItems(offsets: IndexSet) {
+//        withAnimation {
+//            for index in offsets {
+//                modelContext.delete(items[index])
+//            }
+//        }
+//    }
+}
+
+struct AlexaView: View {
+    var body: some View {
+        Text("Hello Alexa")
+    }
+}
+
+struct GoogleView: View {
+    var body: some View {
+        Text("Hey Google")
+    }
+}
+
+#Preview {
+    AssistantSelectView()
+        .modelContainer(for: Assistant.self, inMemory: true)
+}
--- EchoHub/EchoHubApp.swift
+++ EchoHub/EchoHubApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct EchoHubApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Assistant.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
@@ -25,7 +25,7 @@ struct EchoHubApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AssistantSelectView()
         }
         .modelContainer(sharedModelContainer)
     }
