69dfa6d17da0f32620235a445db6eb2fb0ad68e8
Sambujang Fofana <sfofana@ic-tsloan-m126.local>
2024-02-11 07:48:33 -0500

    Updated action delete and action-to-speech

--- EchoHub/ActionsInterfaceView.swift
+++ EchoHub/ActionsInterfaceView.swift
@@ -6,43 +6,50 @@
 //
 
 import SwiftUI
-import SwiftData
+import AVFoundation
 
-struct struct : ActionsInterfaceView {
+struct ActionsInterfaceView: View {
     var body: some View {
-        // Implementation of all the actions
-
-        // Once an action is clicked, the command for that action is converted from action-to-speech
-        
-        // if (assistant == Alexa) {
-            let utterance = AVSpeechUtterance(string: "Alexa, \(/*peform action*/)")
-            utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
-            utterance.rate = 0.3
-
-            let synthesizer = AVSpeechSynthesizer()
-            synthesizer.speak(utterance)
-        // } else {
-            let utterance = AVSpeechUtterance(string: "Hey Google, \(/*peform action*/)")
-            utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
-            utterance.rate = 0.3
-            
-            let synthesizer = AVSpeechSynthesizer()
-            synthesizer.speak(utterance)
-        // }
-
-
-        // Assuming that the actions are in a list format
-        // List {
-            // Section {
-                // Perform delete action
-                .onDelete(perform: delete)
-            // }
-        // }
-
-        func delete(indexSet: IndexSet){
-            // Delete is performed through swiping motion
-            /*actions_list*/.remove(atOffsets: indexSet)
+        // For each action in a category, call a function to convert
+        // the pressed action into speech.
+        // Example VStack containing an action button
+        VStack{
+            Button("Lights"){
+                let command = "turn on the lights."
+                ActionToSpeech(action_command: command)
+            }
         }
+        
+        // Enable a feature to have an option that allows users to
+        // move and delete actions through and edit button
+/*      List {
+            ForEach(actons) {action in Text(action.name)}
+            .onMove(perform: move)
+            .onDelete(perform: deleteItem)
+        }
+            .navigationBarItems(trailing: EditButton())
+ */
     }
+    
+    func ActionToSpeech(action_command: String) {
+        let utterance = AVSpeechUtterance(string: "Hey Google,  \(action_command)")
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        utterance.rate = 0.3
+
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
+    }
+    
+/*  func move(from source: IndexSet, to destination: Int) {
+        actions.move(fromOffsets: source, toOffset: destination)
+    }
+ 
+    func deleteItem(at offsets: IndexSet){
+        characters.remove(atOffsets: offsets)
+    }
+*/
 }
 
+#Preview {
+    ActionsInterfaceView()
+}
