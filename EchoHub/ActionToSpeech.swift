bf3286196c4dbfd28ff302a865cf6fd035a05446
erxclau <ericlau41802@gmail.com>
2024-03-17 17:34:18 -0400

    Configure Polly

--- EchoHub/ActionToSpeech.swift
+++ EchoHub/ActionToSpeech.swift
@@ -7,18 +7,36 @@
 
 import SwiftUI
 import AVFoundation
+import AWSPolly
 
 class ActionToSpeech {
-    init() {
-        AVSpeechSynthesisVoice.speechVoices()
-    }
-    @State var synthesizer = AVSpeechSynthesizer()
+    let player = AVPlayer();
 
     func ActionToSpeech(action_command: String) {
-        let utterance = AVSpeechUtterance(string: "\(action_command)")
-        utterance.voice = AVSpeechSynthesisVoice(language: "en-AU")
-        utterance.rate = 0.5
-        synthesizer.speak(utterance)
+        let input = AWSPollySynthesizeSpeechURLBuilderRequest();
+        input.text = action_command;
+        input.outputFormat = AWSPollyOutputFormat.mp3;
+
+        // TODO: allow user to pick which voice they want...
+        input.voiceId = .aditi;
+
+        // TODO: if the prompt has already been invoked, retrieve from a cache...
+        let builder = AWSPollySynthesizeSpeechURLBuilder.default().getPreSignedURL(input);
+
+        // TODO: if the URL errors, then fall back to default voice...
+        builder.continueOnSuccessWith { (awsTask: AWSTask<NSURL>) -> Any? in
+            // The result of getPresignedURL task is NSURL.
+            // Again, we ignore the errors in the example.
+
+            // TODO: store URL (or actual mp3) in a cache. I believe pre-signed URLs expire
+            let url = awsTask.result!
+
+            // Try playing the data using the system AVAudioPlayer
+            self.player.replaceCurrentItem(with: AVPlayerItem(url: url as URL));
+            self.player.play();
+
+            return nil;
+        }
     }
 }
 
--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Nonverbal individuals have to bridge a gap when communicating. To aid their ability to communicate, we propose an iOS application called EchoHub that integrates with smart home assistants. EchoHub will allow nonverbal users to partner with their choice of home assistant, assign pictures to actionable commands, and have their home assistant execute these commands for them so that they can have greater independence in their daily life. While nonverbal individuals don’t communicate through speech, they can synthesize outside information and express their needs through writing, typing, or physical cues; however, they rely on intermediate individuals to translate their form of communication into an actionable outcome. EchoHub cuts out the middleman and allows users to have greater independence in their daily life.
 
+## Development
+
+If you don't already have CocoaPods, run `sudo gem install cocoapods` and `pod setup`.
+
+1. Run `pod install`.
+2. Open XCode by clicking on `EchoHub.xcworkspace`.
+
 ## Installation
 
 Make sure you have your iPhone running on iOS 17.2 or later. To install and run EchoHub on your local device, you must follow these steps:
